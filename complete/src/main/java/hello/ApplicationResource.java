afba388bfc0a26e10657ceed03ad4310e16778a4
Cassandrea <cassssandrea@gmail.com>
2017-04-28 11:13:26 +0800

    deployable to tomcat on AWS server

---
+++ complete/src/main/java/hello/ApplicationResource.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ApplicationResource {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("/")
+    public Quote getQuote(){
+        Quote quote = restTemplate.getForObject(
+                "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        return quote;
+    }
+}
